765bb4ae718d2582e911ddc275d5c28b6a7f2ad9
Mark Ellul <mark@iplusd.net>
2014-05-08 22:05:52 +0200

    Fix to sign out

--- elements/snippets/fe_set_sign_in_nav.php
+++ elements/snippets/fe_set_sign_in_nav.php
@@ -13,8 +13,13 @@ $core_path = $modx->getOption( 'col_public.core_path', '', MODX_CORE_PATH.'compo
 require_once $core_path.'col-library/col.php';
 
 if ( COL::is_signed_in() ) {
-
-  $sChunk = $modx->getOption( 'tpl', $scriptProperties, 'SignedInNavBar' );
+  if ( isset( $small ) ) {
+    $sChunk = $modx->getOption( 'tpl', $scriptProperties, 'SignedInNavBarSmall' );
+  }
+  else {
+    $sChunk = $modx->getOption( 'tpl', $scriptProperties, 'SignedInNavBar' );
+  }
+  
   return $modx->getChunk( $sChunk, array( 'user_avatar_image_url' => COL::_get_avatar_image(), 'user_username' => COL::_get_name() ) );
 
 } else {

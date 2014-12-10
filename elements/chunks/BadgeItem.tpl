8b9948734466f28439b1c2187cbd8f42648e7479
Tre Everette <tre.everette@gmail.com>
2014-12-10 11:24:15 -0600

    Update badge library to display activities for badges.
    Conflicts:
    	elements/chunks/BadgeItem.tpl

--- elements/chunks/BadgeItem.tpl
+++ elements/chunks/BadgeItem.tpl
@@ -7,10 +7,12 @@
 	</div>
 	<div class="columns item-content" style="height:291px;overflow:hidden">
 	  <h5 style="text-align:center">[[+name]]</h5>
-	  <p style="height:160px; overflow:hidden">[[+description]]</p>
-	  <!--<h6>DO THESE ACTIVITIES</h6>-->
-	  <div style="height:75px"></div>
+	  <p style="height:160px; overflow:hidden;margin-bottom:.1em">[[+informal_description]]</p>
+	  <div style="position:absolute;bottom:0px">
+	  	[[+activity_list]]
+	  </div>
 	</div>
+	
  	<a href="badge-details?id=[[+id]]&ref=bad-lib" class="button small radius" style="position:absolute; bottom:10px; left:20px; right:20px">LEARN MORE</a>
   </div>
 </li>
--- elements/snippets/fe_do_badge_search.php
+++ elements/snippets/fe_do_badge_search.php
@@ -17,12 +17,30 @@ $allBadgesResults = COL::getAllBadges($pg, $catId);
 /* construct other badges */
 $badgeTpl = $modx->getOption( 'tpl', $scriptProperties, 'BadgeItem' );
 $badgeList = "";
+
 foreach ($allBadgesResults['hits']['hits'] as $badgeItem) {
-        $badge = $badgeItem['_source'];
-        $badge["informal_description"]
-                = $badge["informal_description"]=="" ? $badge["description"] : $badge["informal_description"];
-        $output = $modx->getChunk($badgeTpl, $badge);
-        $badgeList .=$output;
+	$badge = $badgeItem['_source'];
+	if(count($badge["activities"]) > 0) {
+		$counter==1;
+		$activity_label = count($badge["activities"]) > 1 ? "DO THESE ACTIVITIES" : "DO THIS ACTIVITY";
+		$activity_html = '<h6>'.$activity_label.'</h6><div style="height:51px;overflow:hidden;">';
+		foreach($badge["activities"] as $activity) {
+
+			// build link based on type ScheduledProgram vs Pathway
+			$link = $activity["activity_type"]=="ScheduledProgram" ? "workshop-detail?ref=bad-lib&id=" : "challenges?ref=bad-lib&id=";
+			$activity_html .= '<a href="' . $link . $activity["id"] . '"><img style="height:48px;margin-right:1px" src="'.$activity["logo_url"].'" title="' . $activity["name"] .'"/></a>';
+			if($counter++>3) break;
+		}
+		$activity_html .="</div>";
+	}
+	$badge["informal_description"]
+	= $badge["informal_description"]=="" ? $badge["description"] : $badge["informal_description"];
+	if($badge["informal_description"].length>160) {
+		$badge["informal_description"] = substr($badge['informal_description'],0,160)."...";
+	}
+	$badge["activity_list"] = $activity_html;
+	$output = $modx->getChunk($badgeTpl, $badge);
+	$badgeList .=$output;
 }
 
 $boilerPlate = '<div class="row"><div class="small-12 columns"><ul id="badge-library-list" class="small-block-grid-1 large-block-grid-4 columns">'. $badgeList . '</ul></div></div>';
